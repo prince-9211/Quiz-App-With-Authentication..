ad67fa606f00acb536b40fb31ebce54aa6dd4eb7
prince kumar sacxena <86111082+prince-9211@users.noreply.github.com>
2022-10-10 12:02:57 +0530

    Update Firebase.js

--- src/authentication/Firebase.js
+++ src/authentication/Firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from 'firebase/auth'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCp6_5oNUg7e24tymvXANUn3f2Ioyd-H6o",
-  authDomain: "authentication-91572.firebaseapp.com",
-  projectId: "authentication-91572",
-  storageBucket: "authentication-91572.appspot.com",
-  messagingSenderId: "758698898505",
-  appId: "1:758698898505:web:590b2c0cf9327828a53b98",
-  measurementId: "G-DQD8Z37LZE"
+  apiKey: "AIzaSyB2IxlU9VtPJY6t4wJ_0BtoCDpWcKHXCss",
+  authDomain: "authentication-2f339.firebaseapp.com",
+  projectId: "authentication-2f339",
+  storageBucket: "authentication-2f339.appspot.com",
+  messagingSenderId: "953199972744",
+  appId: "1:953199972744:web:bb23aa285c1efb09a2aa3d",
+  measurementId: "G-Q993D1V95Q"
 };
 
 const loginButton = initializeApp(firebaseConfig);
